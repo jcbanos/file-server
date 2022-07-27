6bb66d1b2dca6bcc9cf3238d701eecf169f5a075
Juan Carlos Banos <juancarlosbanosmafud@gmail.com>
2022-07-27 09:55:04 -0500

    Refactored and commented server code

--- serv/server.go
+++ serv/server.go
@@ -19,17 +19,24 @@ const (
 	BUFFER = 1024
 )
 
-// Struct for a server. The only attribute is clients a map of slices for io.Writers
+// Struct for a server. The attributes are:
+// clients: a map of slices for net.Conns
+// clientCount: integer to keep count of clients
 type server struct {
     clients map[int][]net.Conn
 	clientCount int
 }
 
+// instance method to add a client to the clients map.
 func (s *server) addClient(c net.Conn, channel int) {
     s.clients[channel] = append(s.clients[channel], c)
 }
 
+
+// Function that will send file to cleints subscribed to a channel
 func (s *server) sendFileToSubscribedClients(fileName string, channel int) {
+
+	// Retrieve list of clients whom I will se
 	clientsToSend := s.clients[channel]
 	fmt.Printf("Clients to send to: %d\n", clientsToSend)
 	for _, client := range clientsToSend{
@@ -38,66 +45,101 @@ func (s *server) sendFileToSubscribedClients(fileName string, channel int) {
 
 }
 
-func (s *server) handleClient(c net.Conn) {
+
+// Function that handles a client on initial connection
+func (s *server) handleClient(conn net.Conn) {
+
+	// Create a buffer
 	recvBuf := make([]byte, BUFFER)
-	_, err := c.Read(recvBuf[:])
+
+	// read from the connection to get client args
+	_, err := conn.Read(recvBuf[:])
 	if err != nil {
 		fmt.Println("Error reading from client connection")
 		os.Exit(1)
 	}
 
-	
+	// Process clients args by removing delimitir : and splitting the string
 	clientArgs := strings.Trim(string(recvBuf[:]), ":")
 	sliceArgs := strings.Split(clientArgs, " ")
+
+	// if client is doing recieve action then handle recieve
 	if sliceArgs[0] == "recieve" {
-		channel, err := strconv.Atoi(strings.Replace(sliceArgs[2], "\x00", "", -1))
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-		strId:= strconv.Itoa(s.clientCount)
-		fmt.Fprintf(c, strId)
-		s.clientCount += 1
-		s.addClient(c, channel)
+		s.handleRecieveClient(conn, sliceArgs)
 	} else if sliceArgs[0] == "send" {
-		fmt.Printf("%v\n", sliceArgs)
-		channel, err := strconv.Atoi(strings.Replace(sliceArgs[2], "\x00", "", -1))
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-		strId:= strconv.Itoa(s.clientCount)
-		fmt.Fprintf(c, strId)
-		s.clientCount += 1
-		fmt.Printf("Client #%d is sending file %s to channel %d\n", s.clientCount, sliceArgs[1], channel)
-		bufferFileSize := make([]byte, 10)
-
-		c.Read(bufferFileSize)
-		fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
-		fmt.Printf("The file size is: %d\n", fileSize)
-
-		newFile, err := os.Create(filepath.Join("files", filepath.Base(sliceArgs[1])))
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		defer newFile.Close()
-		var receivedBytes int64
-
-		for {
-			if (fileSize - receivedBytes) < BUFFER {
-				io.CopyN(newFile, c, (fileSize - receivedBytes))
-				c.Read(make([]byte, (receivedBytes+BUFFER)-fileSize))
-				break
-			}
-			io.CopyN(newFile, c, BUFFER)
-			receivedBytes += BUFFER
-		}
-		fmt.Println("Received file completely!")
-		s.sendFileToSubscribedClients(sliceArgs[1], channel)
+		s.handleSendClient(conn, sliceArgs)
 	}
-}	
+}
+
+// Method to handle clients with recieve keyword
+func (s *server) handleRecieveClient(conn net.Conn, sliceArgs []string){
+
+	// Get channel number from client args
+	channel, err := strconv.Atoi(strings.Replace(sliceArgs[2], "\x00", "", -1))
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	// Get a client Id from clientcount
+	strId:= strconv.Itoa(s.clientCount)
+
+	// Send the client id to client
+	fmt.Fprintf(conn, strId)
+
+	// Increment client count and add client connection/channel to the map
+	s.clientCount += 1
+	s.addClient(conn, channel)
+}
+
+func (s *server) handleSendClient(conn net.Conn, sliceArgs []string){
+	fmt.Printf("%v\n", sliceArgs)
+
+	// Get the channel number
+	channel, err := strconv.Atoi(strings.Replace(sliceArgs[2], "\x00", "", -1))
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	//Get an id for the client send it to client and increment client count
+	strId:= strconv.Itoa(s.clientCount)
+	fmt.Fprintf(conn, strId)
+	s.clientCount += 1
+	fmt.Printf("Client #%d is sending file %s to channel %d\n", s.clientCount, sliceArgs[1], channel)
+
+	// Create buffer to get file size and read connection for file size
+	bufferFileSize := make([]byte, 10)
+	conn.Read(bufferFileSize)
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+	fmt.Printf("The file size is: %d\n", fileSize)
+
+	// Create a file to download the clients file
+	newFile, err := os.Create(filepath.Join("files", filepath.Base(sliceArgs[1])))
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer newFile.Close()
+
+	// Create variable to keep track of how many bytes have been recieved
+	var receivedBytes int64
+	for {
+		if (fileSize - receivedBytes) < BUFFER {
+			io.CopyN(newFile, conn, (fileSize - receivedBytes))
+			conn.Read(make([]byte, (receivedBytes+BUFFER)-fileSize))
+			break
+		}
+		io.CopyN(newFile, conn, BUFFER)
+		receivedBytes += BUFFER
+	}
+	fmt.Println("Received file completely!")
+
+	// After downloading file, send file to clients subscribed to channel
+	s.sendFileToSubscribedClients(sliceArgs[1], channel)
+}
+
 
 func main(){
 
@@ -133,10 +175,20 @@ func main(){
             continue
         }
 
+		defer conn.Close()
+
 		// Handle connection with handleClient Function
 		go srv.handleClient(conn)
 
     }
 }
 
+/* Things I would like to improve:
+
+	-Use a mutex with the server struct since there could be race conditions
+
+	-Instead of writing a concurrent server, use worker threads to make server run in parallel
+
+	-Handle closed connections
+*/
 
